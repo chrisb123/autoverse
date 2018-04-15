70cdd608dca811ab1057b16d4469bcfc36adbff4
Chris <chrisneilbrown@gmail.com>
2018-04-16 01:36:37 +1000

    wasd,rf, RMB
    wasd
    r and f for up and down
    Right Mouse to rotate view

--- Camera.gd
+++ Camera.gd
@@ -1,23 +1,45 @@
 extends Camera
 
-var pos = Vector3(0,5,8)
 const CAMSPEED = 10
+var RMB = false
 
 func _ready():
 	pass
 
 func _process(delta):
-	var x = pos.x
+	var pos = Vector3()
 	if Input.is_action_pressed("ui_right"):
 		pos.x += delta * CAMSPEED
-	if Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left"):
 		pos.x -= delta * CAMSPEED
-	if Input.is_action_pressed("ui_up"):
-		pos.y += delta * CAMSPEED
-	if Input.is_action_pressed("ui_down"):
-		pos.y -= delta * CAMSPEED
-	if Input.is_action_pressed("ui_fwd"):
+	elif Input.is_action_pressed("ui_fwd"):
 		pos.z -= delta * CAMSPEED
-	if Input.is_action_pressed("ui_back"):
+	elif Input.is_action_pressed("ui_back"):
 		pos.z += delta * CAMSPEED
+	elif Input.is_action_pressed("ui_up"):
+		pos.y += delta * CAMSPEED
+	elif Input.is_action_pressed("ui_down"):
+		pos.y -= delta * CAMSPEED
+	translate_object_local(pos)
+	pos = get_translation()
+	if pos.y < 0:
+		pos.y = 0
 	set_translation(pos)
+
+func _input(event):
+	if event is InputEventMouseButton:
+#       print("Mouse Click/Unclick at: ", event.position)
+		if event.get_button_index() == 2 and event.is_pressed():
+			RMB = true
+		elif event.get_button_index() == 2 and not event.is_pressed():
+			RMB = false
+	elif event is InputEventMouseMotion and RMB:
+		var rot = event.get_relative()
+		var camrot = get_rotation_degrees()
+		camrot.y -= rot.x / CAMSPEED
+		camrot.x -= rot.y / CAMSPEED
+		if camrot.x > -5:
+			camrot.x = -5
+		elif camrot.x < -85:
+			camrot.x = -85
+		set_rotation_degrees(camrot)
