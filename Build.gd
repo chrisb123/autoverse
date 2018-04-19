c79e580cd4ebb7ddc8bea56795878ea25b2ef0e7
Chris <chrisneilbrown@gmail.com>
2018-04-19 11:04:47 +1000

    Dictionary handles vectors as keys

--- Build.gd
+++ Build.gd
@@ -7,7 +7,7 @@ extends MenuButton
 var popup
 var placing_item
 var object_to_place
-var object_size #Should probably be a vector2 or 3
+var object_size = Vector3()
 var object_id
 onready var camera = get_node("/root/Main/Camera")
 onready var grid_map = get_node("/root/Main/GridMap")
@@ -25,13 +25,13 @@ func _on_item_pressed(object_id):
 	#print(camera.get_grid())
 	if object_id == 0:
 		object_to_place = factory1.instance()
-		object_size = 2
+		object_size = Vector3(1,0,1)
 	elif object_id == 1:
 		object_to_place = factory2.instance()
-		object_size = 2
+		object_size = Vector3(1,0,1)
 	elif object_id == 2:
 		object_to_place = belt.instance()
-		object_size = 1
+		object_size = Vector3(0,0,0)
 	grid_map.add_child(object_to_place)
 	camera._set_placement(true) #set placment flag in camera
 	placing_item = true
@@ -41,19 +41,17 @@ func _process(delta):
 		_get_grid(object_to_place)
 
 func _get_grid(object_to_place):
-		object_to_place.translation.x = camera._get_grid().x
-		object_to_place.translation.y = camera._get_grid().y
-		object_to_place.translation.z = camera._get_grid().z #should probably use 3 values
+	object_to_place.translation = camera._get_grid()
 		
 func _check_place_object():
-	if grid_map._is_grid_empty(object_to_place.translation.x, object_to_place.translation.z,object_size): # true is always implied when there is no comparison
+	if grid_map._is_grid_empty(object_to_place.translation, object_size): # true is always implied when there is no comparison
 		_place_object_allowed()
 	else:
 		_place_object_error()
 
 func _place_object_allowed():
 	print("----- GRID EMPTY -----")	
-	grid_map._set_grid_contents(object_to_place.translation.x, object_to_place.translation.z, 1, object_size) #todo test value, change to obj id
+	grid_map._set_grid_contents(object_to_place.translation, 1, object_size) #todo test value, change to obj id
 	_reset_build()
 		
 func _place_object_error():
--- GridMap.gd
+++ GridMap.gd
@@ -2,31 +2,27 @@ extends GridMap
 
 # grid map should probably have x,y,z and internal ID
 
-var grid_map = []
+var grid_map = {}
 var height = 10
 var width = 10
 var depth = 2
 
 func _ready():
-	for x in range(height):
-		grid_map.append([])
-		for y in range(height):
-			grid_map[x].append(0)
+	pass
+#	for x in range(height):
+#		grid_map.append([])
+#		for y in range(height):
+#			grid_map[x].append(0)
 
-func _is_grid_empty(x,y,size): #use 'is' not 'get' for boolean requests
-	if grid_map[x][y] == 0 and size == 1: #negative indexes dont work...
-		return true
-	elif grid_map[x][y] == 0 and grid_map[x+1][y] == 0 and grid_map[x][y-1] == 0 and grid_map[x+1][y-1] == 0 and size == 2: #terrible method
-		return true
-	else:
+func _is_grid_empty(pos,size): #use 'is' not 'get' for boolean requests
+	if grid_map.has(pos): #negative indexes dont work...
 		return false
+	else:
+		return true
 
-func _set_grid_contents(x,y,object_to_add,size):
-	grid_map[x][y] = object_to_add
-	if size == 2: #terrible implimentation
-		grid_map[x+1][y] = object_to_add
-		grid_map[x][y-1] = object_to_add
-		grid_map[x+1][y-1] = object_to_add
+func _set_grid_contents(pos,object_to_add,size):
+	grid_map[pos] = object_to_add
+
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
