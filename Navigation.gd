4f6cc8577ca40c29795966681d3bacb8175254b1
Chris <chrisneilbrown@gmail.com>
2018-04-27 15:51:35 +1000

    more other nav stuff, of which does not seem work

--- Navigation.gd
+++ Navigation.gd
@@ -2,11 +2,17 @@ extends Navigation
 
 onready var camera = get_node("/root/Main/Camera")
 
-#func _input(event):
-#	if event is InputEventMouseButton:
-#		if event.get_button_index() == 1 and event.is_pressed():
-#			var result = camera._mouse_ray(event.position)
-#			if result:
-#				var grid_select = camera._int_position(result.position)
-#				print(grid_select)
-#				print(get_closest_point(grid_select)) #should print closest point, but doesnt
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.get_button_index() == 1 and event.is_pressed():
+			var result = camera._mouse_ray(event.position)
+			if result:
+				var grid_select = camera._int_position(result.position)
+				print(grid_select)
+				print(get_closest_point(grid_select)) #should print closest point, but doesnt
+	
+#	print(get_simple_path(Vector3(-10,1,-10),Vector3(10,0,10)))
+	
+	var path = get_simple_path(Vector3(-1,0,-1),Vector3(1,0,1))
+	for a in path:
+		print(a)
--- NavigationMesh.gd
+++ NavigationMesh.gd
@@ -1,11 +1,41 @@
 extends NavigationMeshInstance
 
 var vertices = PoolVector3Array()
+var pia = PoolIntArray()
+var tf = Transform()
 
 func _ready():
 	print("navigationMesh")
-	for x in range(-10,10):
-		for y in range(-10,10):
-			vertices.append(Vector3(x,1,y)) #create a -10 to 10 grid
-	navmesh.set_vertices(vertices) #add grid to nav mesh
-	get_parent().navmesh_add(self,Transform(Vector3(1,0,0),Vector3(0,1,0),Vector3(0,0,1),Vector3(0,0,0))) #add navmesh to navigation
+#	for x in range(-10,10):
+#		for y in range(-10,10):
+#			vertices.append(Vector3(x,1,y)) #create a -10 to 10 grid
+#	navmesh.set_vertices(vertices) #add grid to nav mesh
+#	get_parent().navmesh_add(self,Transform(Vector3(1,0,0),Vector3(0,1,0),Vector3(0,0,1),Vector3(0,0,0))) #add navmesh to navigation
+	
+	pia.append(-10)
+	pia.append(1)
+	pia.append(-10)
+	
+	pia.append(-10)
+	pia.append(1)
+	pia.append(10)
+	
+	pia.append(10)
+	pia.append(1)
+	pia.append(10)
+	
+	pia.append(10)
+	pia.append(1)
+	pia.append(-10)
+	
+	pia.append(-10)
+	pia.append(1)
+	pia.append(-10)
+	
+	print(navmesh.get_polygon_count())
+	navmesh.add_polygon(pia)
+	get_parent().navmesh_add(navmesh,tf)
+#	tf.translated(Vector3(2,3,4))
+#	get_parent().navmesh_add(navmesh,tf)
+	print(navmesh.get_polygon_count())
+	
